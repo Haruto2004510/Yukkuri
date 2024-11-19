56cca173d02c79dc2a1b1e9f4f5227fb14353019
Haruto2004510 <a24.ycgj@g.chuo-u.ac.jp>
2024-11-19 17:23:00 +0900

    Initial Commit

---
+++ Yukkuri/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yukkuri
+//
+//  Created by 山田　悠登  on 2024/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yukkuri/YukkuriApp.swift
@@ -0,0 +1,17 @@
+//
+//  YukkuriApp.swift
+//  Yukkuri
+//
+//  Created by 山田　悠登  on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct YukkuriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
